eb72f0f7758d7539dbe9ccee129524c4786559e4
md-alamin-tech <your_email@example.com>
2025-10-10 23:37:56 +0600

    Practice Bitwise operator

---
+++ lecture_7/Bitwice_and_operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 4, b = 8;
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+    cout << (a ^ b) << endl;
+    cout << (4 << 2) << endl;
+    cout << (4 >> 2) << endl;
+
+    return 0;
+}
