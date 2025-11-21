d280147261a705f3ecda5ed7751416855b2f63aa
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-11-21 23:04:20 +0600

    Update code

--- Math-for-DSA/count_digit.cpp
+++ Math-for-DSA/count_digit.cpp
@@ -1,21 +1,27 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
 
 int printDigit(int n){
     int count = 0;
-    while(n!=0){
+    int sum = 0;
+    while(n!=0){   // TC = (log base 10 ^n)
         int digit = n % 10;
         count++;
+        sum += digit;
         cout << digit << endl;
 
         n = n / 10;
     }
+    cout << "Sum of digit: " << sum << endl;
     return count;
 }
 
 int main(){
     int n = 2546;
-    cout <<"Total count: "<<printDigit(n);
+    cout <<"Total count: "<<printDigit(n) << endl;
+
+    cout << "Count digit in short cut: " << (int)(log10(n) + 1) << endl;
 
     return 0;
 }
