061a3632720be072bdcf88f8760f647e568e2271
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-11-22 00:07:06 +0600

    this code exicute is this number an armstrong or not

---
+++ Math-for-DSA/armStrong_number.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+bool isArmstrong(int n){
+    int copyN = n;
+    int sumOfCubes = 0;
+
+    while (n != 0)
+    {
+        int dig = n % 10;
+        sumOfCubes += (dig * dig * dig);
+
+        n = n / 10;
+    }
+    return sumOfCubes == copyN;
+}
+int main(){
+    int n = 153;
+
+    if(isArmstrong(n)){
+        cout << "Is an armStrong number\n";
+    }else{
+        cout << "Not an armstrong number\n";
+    }
+
+    return 0;
+}
