c5dd6b1056c0e89819597ad128b2067d49996bf2
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-11-25 00:27:31 +0600

    Reverse int number

---
+++ Math-for-DSA/reverse_number.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int reversInt(int n){
+    long long revNum = 0;
+
+    while(n != 0){
+        int dig = n % 10;
+
+        if(revNum > INT_MAX/10 || revNum < INT_MIN/10){
+            return 0;
+        }
+
+        revNum = revNum * 10 + dig;
+        n = n / 10;
+    }
+    return revNum;
+}
+
+int main(){
+    int n = 254;
+    cout << reversInt(n) << endl;
+
+    return 0;
+}
