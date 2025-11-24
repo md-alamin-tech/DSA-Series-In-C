9ea80d79b071becf03487cb1b3cc7b275ad05de2
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-11-24 20:37:17 +0600

    This code also find GCD and LCM

---
+++ Math-for-DSA/LCM.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    while(a>0 && b>0){
+        if(a>b){
+            a = a % b;
+        }else{
+            b = b % a;
+        }
+    }
+    if(a==0) return b;
+    return a;
+}
+
+int lcm(int a, int b){
+    int hcfOrGcd = gcd(a, b);
+    return (a * b) / hcfOrGcd;
+}
+
+int main(){
+    cout << lcm(20, 28) << endl;
+    return 0;
+}
