94f8c9b09b66f96acb76100c8b5574ac47c38e30
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-11-30 23:08:14 +0600

    change index value

--- 2D-Arrays/create-matrix.cpp
+++ 2D-Arrays/create-matrix.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(){
-    int matrix[4][3] = {{1,2,3}, {4,5,6}, {7,8,9,},{10,11,12}};
+    int matrix[4][3] = {{1,2,3}, {4,5,6}, {7,8,9},{10,11,12}};
 
     int rows = 4;
     int cols = 3;
 
+    matrix[2][1] = 18;
     cout << matrix[2][1] << endl;
 
     return 0;
