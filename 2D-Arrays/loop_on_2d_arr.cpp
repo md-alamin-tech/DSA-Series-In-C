b340c05c786d47408dad8e4e40e0f9c72e901ca7
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-11-30 23:14:20 +0600

    use loop in 2d arr

---
+++ 2D-Arrays/loop_on_2d_arr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    int rows = 3;
+    int cols = 3;
+
+    for (int i = 0; i < rows;i++){
+        for(int j = 0; j < cols; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
