5bfa91579bac401ae02b5a63f4e174de0c6491e1
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-11-30 23:31:23 +0600

    Update code

--- 2D-Arrays/loop_on_2d_arr.cpp
+++ 2D-Arrays/loop_on_2d_arr.cpp
@@ -2,16 +2,25 @@
 using namespace std;
 
 int main() {
-    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int arr[3][3];
 
     int rows = 3;
     int cols = 3;
 
-    for (int i = 0; i < rows;i++){
-        for(int j = 0; j < cols; j++){
-            cout << arr[i][j] << " ";
+    //input
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            cin >> arr[i][j];
         }
-        cout << endl;
     }
+    //output
+    for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
     return 0;
 }
