9738c4f6e81cbfcab2a7133747bdd82909e0eb67
md-alamin-tech <contact.alamin.tech@gmail.com>
2026-02-14 22:47:35 +0600

    input value in 2D array

--- 2D-Arrays/create_matrix.cpp
+++ 2D-Arrays/create_matrix.cpp
@@ -2,21 +2,27 @@
 using namespace std;
 
 int main(){
-    int matrix[4][3] = {{1,2,3}, {4,5,6}, {7,8,9},{10,11,12}};
+    int matrix[4][3];
 
     int rows = 4;
     int cols = 3;
 
-    cout << matrix[2][1] << endl;
-
-    for (int i = 0; i < rows; i++)
-    {
+    for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++){
-            cout << matrix[i][j] << "  " ;
+            cin >> matrix[i][j];
         }
         cout << endl;
     }
 
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                cout << matrix[i][j] << "  ";
+            }
+            cout << endl;
+        }
+
     
-        return 0;
+    return 0;
 }
