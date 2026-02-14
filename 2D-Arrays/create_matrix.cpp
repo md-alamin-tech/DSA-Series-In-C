4957ab6563ad9915be5e9491e8fbecd1d7745cd5
md-alamin-tech <contact.alamin.tech@gmail.com>
2026-02-14 23:07:12 +0600

    perform linear search in 2D array

--- 2D-Arrays/create_matrix.cpp
+++ 2D-Arrays/create_matrix.cpp
@@ -2,27 +2,38 @@
 using namespace std;
 
 int main(){
-    int matrix[4][3];
+    int matrix[4][3] = {{1, 4, 5}, {7, 8, 9}, {11, 44, 77}, {12, 3, 22}};
 
     int rows = 4;
     int cols = 3;
+    int key = 11;
 
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++){
-            cin >> matrix[i][j];
-        }
-        cout << endl;
-    }
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                cout << matrix[i][j] << "  ";
+            if(matrix[i][j] == key){
+                cout << matrix[i][j] << endl;
+                cout << i <<" " << j;
+                return 0;
             }
-            cout << endl;
         }
+              
+    }
+    cout << -1;
+    // for (int i = 0; i < rows; i++){
+    //     for (int j = 0; j < cols; j++){
+    //         cin >> matrix[i][j];
+    //     }
+    //     cout << endl;
+    // }
 
-    
+    //     for (int i = 0; i < rows; i++)
+    //     {
+    //         for (int j = 0; j < cols; j++)
+    //         {
+    //             cout << matrix[i][j] << "  ";
+    //         }
+    //         cout << endl;
+    //     }
+
     return 0;
 }
