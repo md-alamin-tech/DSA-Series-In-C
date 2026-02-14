9254a1c66fc9700ab01542a4a6ac96b028ef07fb
md-alamin-tech <contact.alamin.tech@gmail.com>
2026-02-14 23:41:24 +0600

    find max sum row in 2D aray

--- 2D-Arrays/create_matrix.cpp
+++ 2D-Arrays/create_matrix.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
+#include <climits>
 using namespace std;
+
+int getRowSum(int matrix[4][3], int rows, int cols){
+
+    int maxRowSum = INT_MIN;
+    for (int i = 0; i < rows; i++){
+        int rowSumI = 0;
+        for (int j = 0; j < cols; j++){
+            rowSumI += matrix[i][j];
+        }
+
+        maxRowSum = max(rowSumI, maxRowSum);
+    }
+
+    return maxRowSum;
+}
 
 int main(){
     int matrix[4][3] = {{1, 4, 5}, {7, 8, 9}, {11, 44, 77}, {12, 3, 22}};
@@ -8,17 +24,19 @@ int main(){
     int cols = 3;
     int key = 11;
 
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++){
-            if(matrix[i][j] == key){
-                cout << matrix[i][j] << endl;
-                cout << i <<" " << j;
-                return 0;
-            }
-        }
+    cout << getRowSum(matrix, rows, cols) << endl;
+
+    // for (int i = 0; i < rows; i++){
+    //     for (int j = 0; j < cols; j++){
+    //         if(matrix[i][j] == key){
+    //             cout << matrix[i][j] << endl;
+    //             cout << i <<" " << j;
+    //             return 0;
+    //         }
+    //     }
               
-    }
-    cout << -1;
+    // }
+    // cout << -1;
     // for (int i = 0; i < rows; i++){
     //     for (int j = 0; j < cols; j++){
     //         cin >> matrix[i][j];
